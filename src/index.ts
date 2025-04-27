87664e84165d09d108a94daf3fa8a8fcbe47c9d8
Omar <Omar@example.com>
2025-04-27 14:08:35 +0300

    Notes Schema

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import noteRoute from "./routes/noteRoute";
 const app = express();
 const port = 3000;
 
@@ -14,6 +15,8 @@ mongoose
   .connect(process.env.DATABASE_URL || "")
   .then(() => console.log("Mongo connected !"))
   .catch((err) => console.log("Failed to connect !", err));
+
+app.use("/notes", noteRoute);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
---
+++ src/models/noteModel.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface INote extends Document {
+  title: string;
+  content: string;
+  createdDate: Date;
+  updatedDate: Date;
+}
+
+const noteSchema = new Schema<INote>({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  createdDate: { type: Date, required: true },
+  updatedDate: { type: Date, required: true },
+});
+
+const noteModel = mongoose.model<INote>("Note", noteSchema);
+export default noteModel;

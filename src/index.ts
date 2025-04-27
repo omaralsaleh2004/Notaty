b3836abbe3bff96a330945a6f261179901d1cc36
Omar <Omar@example.com>
2025-04-27 13:42:11 +0300

    initial Commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.DATABASE_URL || "")
+  .then(() => console.log("Mongo connected !"))
+  .catch((err) => console.log("Failed to connect !", err));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});

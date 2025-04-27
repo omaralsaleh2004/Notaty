2a9e22c6f4b59b24f647d2e28891aeb972f5057e
Omar <Omar@example.com>
2025-04-27 16:06:08 +0300

    getNote by id feature & get all notes

--- src/routes/noteRoute.ts
+++ src/routes/noteRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import noteModel from "../models/noteModel";
+import mongoose, { ObjectId } from "mongoose";
 
 const router = express.Router();
 
-router.post("/addNote", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { title, content } = req.body;
     const newNote = new noteModel({
@@ -15,6 +16,32 @@ router.post("/addNote", async (req, res) => {
     console.log("req.body:", req.body);
     await newNote.save();
     res.send(newNote);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Someting went wrong !");
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const notes = await noteModel.find();
+    res.status(200).send(notes);
+  } catch (err) {
+    res.status(500).send("Someting went wrong !");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    let note = await noteModel.findById(id);
+    if (!note) {
+      res.status(404).send("the note does not exist !");
+      return;
+    }
+
+    res.status(200).send(note);
   } catch (err) {
     console.log(err);
     res.status(500).send("Someting went wrong !");
